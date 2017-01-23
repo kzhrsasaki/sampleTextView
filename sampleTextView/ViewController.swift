d00e3926e5252847cd4f357f2b5ace4a81c5af32
kzhrsasaki <kzhr.sasaki@gmail.com>
2017-01-23 20:17:24 +0900

    2017/01/23 iOSコース宿題

--- sampleTextView/ViewController.swift
+++ sampleTextView/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var myTextView: UITextView!
 
+    @IBOutlet weak var closeButton: UIButton!
+    
+    // 画面が起動して立ち上がった時発動
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("画面起動しました")
+        myTextView.text = "Hello"
+        myTextView.textColor = UIColor.blue
+        myTextView.font = UIFont(name: "AmericanTypewriter", size: 20)
+        myTextView.textAlignment = NSTextAlignment.center
+        
+        // TODO:myTextViewにHelloを10個表示してください
+        // for文使いましょう
+        for i in 1...10 {
+            myTextView.text = myTextView.text + "Hello"
+        }
+        
+        // TODO: こんにちはnで10個表示してください
+        // P.107 while文使いましょう
+        var n = 0
+        while n < 10 {
+            myTextView.text = myTextView.text + "こんにちは\(n)"
+            n = n + 1
+        }
     }
 
+    // closeボタンが押されたときに発動
+    @IBAction func tapClose(_ sender: UIButton) {
+        //キーボードを閉じる
+        myTextView.resignFirstResponder()
+    }
+    
+    // 使用しているデバイスでメモリ不足を感知した時発動
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
